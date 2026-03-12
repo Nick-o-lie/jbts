ea1b7ee1fcb84b35f6a1debfef0da915eecf2578
Nick-o-lie <129188463+Nick-o-lie@users.noreply.github.com>
2026-03-12 21:29:17 +0300

    Made vertical, fixed background

--- game.gd
+++ game.gd
@@ -1,5 +1,8 @@
-extends AnimatableBody2D
+extends Node2D
 
+func _input(event):
+	if event is InputEventScreenTouch and event.is_pressed():
+		print("Touch position (Viewport): ", event.position)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,5 +11,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# position.x += delta * 1000
 	pass
